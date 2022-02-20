f96d1e192719efb8e93382d6bd4fa59c563ec058
EstebanMoyolema <82001634+EstebanMoyolema@users.noreply.github.com>
2022-02-20 17:56:57 -0500

    Cambio de rutas se agregaron todas las rutas del administrador

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,30 @@ Route::group(['middleware' => ['auth']], function () {
 Route::group(['middleware' => ['auth', 'role:administrator']], function () {
     Route::get('/dashboard/register', [DashboardController::class, 'register'])->name('auth.register');
 });
+/* Ruta para redirigir al registro de rutas desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/rutas', [DashboardController::class, 'rutas'])->name('admin.rutasDash');
+});
+/* Ruta para redirigir al registro de clientes desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/clientes', [DashboardController::class, 'clientes'])->name('admin.clienteDash');
+});
+/* Ruta para redirigir al registro de crear categorias desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/CategoriaProducto', [DashboardController::class, 'catProductos'])->name('admin.catProductosDash');
+});
+/* Ruta para redirigir al registro de productos desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/Productos', [DashboardController::class, 'productos'])->name('admin.productosDash');
+});
+/* Ruta para redirigir al registro de proveedores desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/Proveedores', [DashboardController::class, 'proveedores'])->name('admin.proveedoresDash');
+});
+/* Ruta para redirigir al registro de pedidos desde el admin */
+Route::group(['middleware' => ['auth', 'role:administrator']], function () {
+    Route::get('/dashboard/Pedidos', [DashboardController::class, 'pedidos'])->name('admin.pedidosDash');
+});
 
 
 require __DIR__.'/auth.php';
